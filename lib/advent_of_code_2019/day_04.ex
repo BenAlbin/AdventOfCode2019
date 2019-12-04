144362c26ae5dc83e963f07a2fa84a3a68fc5be3
Ben Albin <ben.g.albin@gmail.com>
2019-12-04 15:55:55 +0000

    Day 4 solutions

--- lib/advent_of_code_2019/day_04.ex
+++ lib/advent_of_code_2019/day_04.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode2019.Day04 do
   def part1(input) do
+    input
+    |> String.split("-")
+    |> Enum.map(&String.to_integer/1)
+    |> (&apply(Range, :new, &1)).()
+    |> Enum.count(
+      fn number ->
+        digits = Integer.digits(number)
+        increasing?(digits, []) && has_pair?(digits)
+      end
+    )
   end
 
   def part2(input) do
+    input
+    |> String.split("-")
+    |> Enum.map(&String.to_integer/1)
+    |> (&apply(Range, :new, &1)).()
+    |> Enum.count(
+      fn number ->
+        digits = Integer.digits(number)
+        increasing?(digits, []) && has_pair_not_in_larger_group?(digits)
+      end
+    )
+  end
+
+  defp increasing?([], _), do: true
+  defp increasing?([next_char | rest], []), do: increasing?(rest, [next_char])
+  defp increasing?([next_char | rest], [previous_char | _ ] = previous) when next_char >= previous_char, do: increasing?(rest, [next_char | previous])
+  defp increasing?(_, _), do: false
+
+  defp has_pair?(digits) do
+    digits
+    |> Enum.chunk_by(& &1)
+    |> Enum.any?(& length(&1) >= 2)
+  end
+
+  defp has_pair_not_in_larger_group?(digits) do
+    digits
+    |> Enum.chunk_by(& &1)
+    |> Enum.any?(& length(&1) === 2)
   end
 end
--- test/advent_of_code_2019/day_04_test.exs
+++ test/advent_of_code_2019/day_04_test.exs
@@ -1,21 +1,29 @@
 defmodule AdventOfCode2019.Day04Test do
   use ExUnit.Case
 
-  import AdventOfCode2019.Day24
+  import AdventOfCode2019.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "111111-111111"
     result = part1(input)
 
-    assert result
+    assert result === 1
+
+    input = "223450-223450"
+    result = part1(input)
+
+    assert result === 0
+
+    input = "123789-123789"
+    result = part1(input)
+
+    assert result === 0
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "111111-111111"
     result = part2(input)
 
-    assert result
+    assert result === 0
   end
 end
